0a3c83e8277e903d0b494d9fa08016dd8f1e5bb3
Tanya Sharma <tanya.sharma@modulrfinance.com>
2026-05-08 16:32:56 +0530

    fixes

--- app/src/test/java/com/project/back_end/BackEndApplicationTests.java
+++ app/src/test/java/com/project/back_end/BackEndApplicationTests.java
@@ -1,10 +1,15 @@
 package com.project.back_end;
 
+import com.project.back_end.repo.PrescriptionRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class BackEndApplicationTests {
+
+	@MockBean
+	private PrescriptionRepository prescriptionRepository;
 
 	@Test
 	void contextLoads() {
